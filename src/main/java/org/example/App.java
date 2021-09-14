56f6d7cf2925ccca8743025e99ea6d22f3145819
Jack Nelson <pl224701@knights.ucf.edu>
2021-09-13 21:29:02 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Jack Nelson
+ */
+
+package org.example;
+import java.math.BigDecimal;
+import java.util.Scanner;
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the password? ");
+        String str = sc.nextLine();
+        if(str.equals("abc$123"))
+            System.out.print("Welcome!");
+        else
+            System.out.print("I don't know you");
+    }
+}
